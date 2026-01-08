eeb0227f404d30c5f3fb2f90d9be8db613306d39
Anh Tai <nguyenhuuanhtai23@gmail.com>
2026-01-08 20:44:20 +0700

    Initialize Project Quiz-app

---
+++ src/main/java/com/app/quiz_app_demo/TestController/NewAPI.java
@@ -0,0 +1,10 @@
+package com.app.quiz_app_demo.TestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class NewAPI {
+    @GetMapping("/test")
+    public String testAPI(){
+        return "success";
+    }
+}
